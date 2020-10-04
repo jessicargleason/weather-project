277b835f659a1b17f8e1062ea9d93a21b1498cc5
Jessica Gleason <jessicaruthgleason@gmail.com>
2020-10-03 19:51:16 -0500

    Better code comments, setting weather code as body class

--- weather-project/src/components/Weather.js
+++ weather-project/src/components/Weather.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React,  { useEffect } from 'react';
 import {useWeatherData} from '../useWeatherData.js';
 
 export default function Weather(props) {
+    //go get weather data from API
     const weatherData = useWeatherData(props.city);
     let weather;
     let temp;
+    let weatherCode;
+    const body = document.getElementsByTagName("body");
     if (weatherData !== null) {
+      //pull needed weather information from the data retrieved from the API
       weather = weatherData["weather"][0]["main"];
       temp = Math.floor(weatherData["main"]["temp"]);
+      weatherCode = weatherData["weather"][0]["id"];
     }
+    useEffect(() => {
+        // Update the document title
+        document.title = `${props.city} Weather`;
+        //Set the numerical weather code as the body class for styling
+        if (weatherCode !== undefined) {
+            body[0].classList = weatherCode;
+        }
+      });
     return (
       <>
       {weatherData !== null &&
--- weather-project/src/useWeatherData.js
+++ weather-project/src/useWeatherData.js
@@ -4,14 +4,17 @@ export function useWeatherData(city) {
     const [weather, setWeather] = useState(null);
 
     useEffect(() => {
+        //when AJAX call is done, update state with weather data
         function handleWeatherChange(weather) {
             setWeather(weather);
         }
 
+        //get weather from API using provided city name
         fetch(`//api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}&units=imperial`)
         .then(response => response.json())
         .then(data => {
             console.log(data);
+            //make sure the location is found before updating data
             if (data["cod"] === 200) {
                 handleWeatherChange(data);
             } else {
