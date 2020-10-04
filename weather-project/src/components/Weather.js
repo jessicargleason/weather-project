3e9cb154463759e576d0e228c1550ec6202fdace
Jessica Gleason <jessicaruthgleason@gmail.com>
2020-10-04 18:48:10 -0500

    Added rain, clouds and cloud animation

--- weather-project/src/components/Weather.js
+++ weather-project/src/components/Weather.js
@@ -7,6 +7,7 @@ export default function Weather(props) {
     let weather;
     let temp;
     let weatherCode;
+    let isPrecipitation = false;
     const body = document.getElementsByTagName("body");
     if (weatherData !== null) {
       //pull needed weather information from the data retrieved from the API
@@ -14,22 +15,50 @@ export default function Weather(props) {
       temp = Math.floor(weatherData["main"]["temp"]);
       weatherCode = weatherData["weather"][0]["id"];
     }
+    if (
+      weatherCode !== undefined && (
+        /* Thunderstorm */ weatherCode.toString().startsWith("2") ||  
+        /* Drizzle */ weatherCode.toString().startsWith("3") ||
+        /* Rain */ weatherCode.toString().startsWith("5") ||
+        /* Snow */ weatherCode.toString().startsWith("6")
+        )
+      ) {
+        isPrecipitation = true;
+      }
     useEffect(() => {
         // Update the document title
         document.title = `${props.city} Weather`;
         //Set the numerical weather code as the body class for styling
         if (weatherCode !== undefined) {
-            body[0].setAttribute("data-weather",weatherCode);
+          body[0].setAttribute("data-weather",weatherCode);
         } else {
           body[0].setAttribute("data-weather","");
         }
       });
     return (
+      <>
       <div className="weather">
       {weatherData !== null
           ? <p>{temp}&deg;F and {weather}</p>
           : <p>Weather Unknown</p>
       }
       </div>
+      {isPrecipitation &&
+        <div className="precip" aria-hidden="true">
+          <span className="drop"></span>
+          <span className="drop"></span>
+          <span className="drop"></span>
+          <span className="drop"></span>
+          <span className="drop"></span>
+          <span className="drop"></span>
+          <span className="drop"></span>
+          <span className="drop"></span>
+          <span className="drop"></span>
+          <span className="drop"></span>
+          <span className="drop"></span>
+          <span className="drop"></span>
+        </div>
+      }
+      </>
     )
   }
